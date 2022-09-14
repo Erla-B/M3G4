0ce8193d72a7d2f4234be19cede11aec8fb2acb3
Erla-B <erlabaldurz@gmail.com>
2022-09-14 11:28:13 +0000

    Added the people

---
+++ index.js
@@ -0,0 +1,38 @@
+// Array containing objects
+let people = [
+    {
+        name : 'Hatty Orriss',
+        dateOfBirth : '5/15/1993',
+        children : 0,
+        country : 'Portugal',
+        knowsHowToProgram : true,
+    },
+    {
+        name : 'Lib Ivankovic',
+        dateOfBirth : '4/12/1983',
+        children : 1,
+        country : 'Poland',
+        knowsHowToProgram : false,
+    },
+    {
+        name : 'Hector Grinyov',
+        dateOfBirth : '12/24/1989',
+        children : 0,
+        country : 'China',
+        knowsHowToProgram : false,
+    },
+    {
+        name : 'Leroy Cast',
+        dateOfBirth : '5/15/1990',
+        children : 4,
+        country : 'Russia',
+        knowsHowToProgram : false,
+    },
+    {
+        name : 'Rock McClary',
+        dateOfBirth : '12/11/1982',
+        children : 1,
+        country : 'China',
+        knowsHowToProgram : true,
+    },
+]
